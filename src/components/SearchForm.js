57be395bcc24ed4960111639fabedf746896a666
buseugrs <sekerbuse_96@hotmail.com>
2024-07-01 15:10:12 +0300

    searchform and userprofile

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUser } from "../features/githubSlice";
 
 const SearchForm = () => {
-  return (
-    <div>SearchForm</div>
-  )
-}
+  const dispatch = useDispatch(); // to achieve Redux store
+  const [username, setUsername] = useState("");
 
-export default SearchForm
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username.trim()) {
+      dispatch(fetchUser(username));
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter GitHub User Name"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchForm;
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const UserProfile = () => {
-  return (
-    <div>UserProfile</div>
-  )
-}
+  const user = useSelector((state) => state.github.user);
 
-export default UserProfile
+  if (!user) {
+    return <div>User not found.</div>;
+  }
+
+  return (
+    <div>
+      <h2>User Information</h2>
+      <div>
+        <img src={user.avatar_url} alt="Avatar" />
+        <h3>{user.login}</h3>
+        <p>Followers: {user.followers}</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;

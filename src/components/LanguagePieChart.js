9f73a969cd42735cb01d81b8a5c751d920978487
buseugrs <sekerbuse_96@hotmail.com>
2024-07-01 14:56:50 +0300

    creating components and redux slice

---
+++ src/components/LanguagePieChart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LanguagePieChart = () => {
+  return (
+    <div>LanguagePieChart</div>
+  )
+}
+
+export default LanguagePieChart
---
+++ src/features/githubSlice.js
@@ -0,0 +1,47 @@
+// Management the GitHub user's infos and repo list.
+
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { act } from "react";
+
+export const fetchUser = createAsyncThunk(
+  "github/fetchUser", //sliceName/actionName-- action name
+  async (username) => {
+    const userResponse = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+    const reposResponse = await axios.get(userResponse.data.repos_url);
+    return { user: userResponse.data, repos: reposResponse.data };
+  }
+);
+
+const githubSlice = createSlice({
+  name: "github", // name for slice
+  initialState: {
+    user: null,
+    repos: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {}, // defining actions
+  extraReducers: {
+    [fetchUser.pending]: (state) => {
+      // state update for fetchUser.pending status
+      state.loading = true;
+      state.error = null;
+    },
+    [fetchUser.fulfilled]: (state, action) => {
+      // state update for fetchUser.fulfilled status
+      state.loading = false;
+      state.user = action.payload.user;
+      state.repos = action.payload.repos;
+    },
+    [fetchUser.rejected]: (state, action) => {
+      // state update for fetchUser.rejected status
+      state.loading = false;
+      state.error = action.error.message;
+    },
+  }, // specify how actions coming from async or other slices will be processed
+});
+
+export default githubSlice.reducer;
